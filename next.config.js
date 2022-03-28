1828b0a29153512fd063f6af80824be8830b8429
Moeen35245 <mohammedmoeen35245@gmail.com>
2022-03-28 23:01:38 +0530

    new changes

--- next.config.js
+++ next.config.js
@@ -3,5 +3,7 @@ module.exports = {
   env: {
     USER_NAME: "moeen8766",
     PASSWORD: "mohammed8766",
+    NEXTAUTH_URL: "https://dsa450-l34ei937d-moeen35245.vercel.app/",
+    NEXTAUTH_SECRET: "ricecIkhkKN2zI8u/0Euxg0CHVQ0INXwfLkEHVawBGk=",
   },
 };
