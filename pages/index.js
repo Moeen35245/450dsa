7572ea5e644cc25a9ccc189b387b6395c976d892
Moeen35245 <mohammedmoeen35245@gmail.com>
2022-04-03 01:05:04 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home(props) {
       </Head>
       <Header />
       <div className="flex">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-[1160px] mx-auto">
+        <div className="grid grid-cols-1 new md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-[1160px] mx-auto">
           {unique.map((item, i) => {
             return (
               <Card
