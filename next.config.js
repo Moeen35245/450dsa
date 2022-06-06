074fc8e7414f1941a96a78af037f7ae81f1c5118
Moeen35245 <70256504+Moeen35245@users.noreply.github.com>
2022-06-06 22:04:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    USER_NAME: "",
-    PASSWORD: "",
-    NEXTAUTH_URL: "",
-    NEXTAUTH_SECRET: "",
+    USER_NAME: "moeen8766",
+    PASSWORD: "mohammed8766",
+    NEXTAUTH_URL: "https://dsa450-l34ei937d-moeen35245.vercel.app/",
+    NEXTAUTH_SECRET: "ricecIkhkKN2zI8u/0Euxg0CHVQ0INXwfLkEHVawBGk=",
   },
 };
