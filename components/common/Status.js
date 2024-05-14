a4056fa71acb88477737b993bbef538c8bd92a29
Moin8766 <gulam.moinuddin@solutionwise.in>
2024-05-14 12:55:19 +0530

    Status changed

--- components/common/Status.js
+++ components/common/Status.js
@@ -1,8 +1,8 @@
 import { LockClosedIcon } from "@heroicons/react/solid";
 function Status() {
   return (
-    <div className="flex items-center justify-around rounded-full w-[85px] h-6 bg-lightRed">
-      <p className="text-xs text-darkRed font-medium">pending</p>
+    <div className="flex items-center justify-around rounded-full w-[85px] h-6 bg-yellow-500/20">
+      <p className="text-xs text-yellow-500 font-medium">pending</p>
     </div>
   );
 }
