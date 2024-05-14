45cf13c29a155b83b9a6a12ad050876959621258
Moeen35245 <mohammedmoeen35245@gmail.com>
2024-05-14 13:31:56 +0530

    fixes build

--- pages/api/auth/signUp.js
+++ pages/api/auth/signUp.js
@@ -1,33 +1,33 @@
-import { hashPassword } from "../../../lib/bcrypt";
-import { connectToDatabase } from "../../../lib/db";
+// import { hashPassword } from "../../../lib/bcrypt";
+// import { connectToDatabase } from "../../../lib/db";
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
+// export default async function handler(req, res) {
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ message: "Method Not Allowed" });
+//   }
 
-  const { email, password } = req.body;
+//   const { email, password } = req.body;
 
-  try {
-    // Check if the email already exists in the database
-    const db = await connectToDatabase();
-    const existingUser = await db.collection("users").findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" });
-    }
+//   try {
+//     // Check if the email already exists in the database
+//     const db = await connectToDatabase();
+//     const existingUser = await db.collection("users").findOne({ email });
+//     if (existingUser) {
+//       return res.status(400).json({ message: "Email already exists" });
+//     }
 
-    // Hash the password
-    const hashedPassword = await hashPassword(password);
+//     // Hash the password
+//     const hashedPassword = await hashPassword(password);
 
-    // Store the user in the database
-    await db.collection("users").insertOne({
-      email,
-      password: hashedPassword,
-    });
+//     // Store the user in the database
+//     await db.collection("users").insertOne({
+//       email,
+//       password: hashedPassword,
+//     });
 
-    return res.status(201).json({ message: "User created successfully" });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
-}
+//     return res.status(201).json({ message: "User created successfully" });
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ message: "Internal Server Error" });
+//   }
+// }
